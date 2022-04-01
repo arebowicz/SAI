cb209344c268084ce46a0ac0a748048fb9f93878
arebowicz <arebowicz@gmail.com>
2022-04-01 12:01:47 +0200

    Last Digit Of A Huge Number -- first version: 884 tests passed, 7 tests failed

---
+++ Last Digit Of A Huge Number/lastDigit.c
@@ -0,0 +1,60 @@
+#include <assert.h>
+
+/*  x^y                  */
+/* 0 <= x,y <= 99        */
+/* 0 <= return val <= 99 */
+int twoLastDigits(int x, int y) {
+  if(y == 0)
+    return 1;
+  if(x == 0 || x == 1)
+    return x;
+  int i;
+  int result = x; // 0 <= result <= 99
+  for(i=0; i<y-1; ++i)
+    result = (result%10)*(x%10) + 
+             10*((result/10)%10)*(x%10) +
+             (result%10)*(x/10)*10;
+  return result%100;
+}
+
+int lastDigit(const unsigned long int *arr, int arr_size) {
+  if(arr_size == 0)
+    return 1;
+  if(arr_size == 1)
+    return arr[0]%10;
+  if(arr_size == 2)
+    return twoLastDigits(arr[0]%100, arr[1]%100)%10;
+  int i, result;
+  result = arr[arr_size-1];
+  for(i=arr_size-1; i>0; --i)
+    result = twoLastDigits(arr[i-1]%100, result%100);
+  return result%10;
+}
+
+int main(int argc, char *argv[]) {
+  const unsigned long arr0[] = {};
+  assert(lastDigit(arr0, 0) == 1);
+  const unsigned long arr1[] = { 0, 0 };
+  assert(lastDigit(arr1, 2) == 1);
+  const unsigned long arr2[] = { 0, 0, 0 };
+  assert(lastDigit(arr2, 3) == 0);
+  const unsigned long arr3[] = { 1, 2 };
+  assert(lastDigit(arr3, 2) == 1);
+  const unsigned long arr4[] = { 3, 4, 5 };
+  assert(lastDigit(arr4, 3) == 1);
+  const unsigned long arr5[] = { 4, 3, 6 };
+  assert(lastDigit(arr5, 3) == 4);
+  const unsigned long arr6[] = { 7, 6, 21 };
+  assert(lastDigit(arr6, 3) == 1);
+  const unsigned long arr8[] = { 2, 2, 2, 0 };
+  assert(lastDigit(arr8, 4) == 4);
+  const unsigned long arr9[] = { 937640, 767456, 981242 };
+  assert(lastDigit(arr9, 3) == 0);
+  const unsigned long arr10[] = { 123232, 694022, 140249 };
+  assert(lastDigit(arr10, 3) == 6);
+  const unsigned long arr11[] = { 499942, 898102, 846073 };
+  assert(lastDigit(arr11, 3) == 6);
+  const unsigned long arr7[] = { 12, 30, 21 };
+  assert(lastDigit(arr7, 3) == 6);
+  return 0;
+}
