45d0bb2f1d5266798439b2c1cecc4219a93ac405
arebowicz <arebowicz@gmail.com>
2022-04-05 16:11:53 +0200

    Gap In Primes -- implemented binary search function

--- Gap In Primes/gap.c
+++ Gap In Primes/gap.c
@@ -2,6 +2,7 @@
 
 #include <stdbool.h>
 #include <math.h>
+#include <stdlib.h>
 
 #include <stdio.h>
 
@@ -11,7 +12,7 @@
 #define NUM_OF_PRIMES 100000
 
 static int PRIMES[NUM_OF_PRIMES];
-static int N = 0;
+static int N = 0;   /* actual NUM_OF_PRIMES */
 static bool isPRIMESInitialized = false;
 
 static void initPRIMES() {
@@ -31,14 +32,50 @@ static void initPRIMES() {
   isPRIMESInitialized = true;
 }
 
+static enum returnIndex { PREDECESSOR, SUCCESOR };
+
+/* searching for first PRIME >= value */
+int binarySearchInPRIMES(int value, enum returnIndex which) {
+  int i, j, center;
+  i = 0;
+  j = N-1;
+  while(i <= j) {
+    center = (i + j) / 2;
+    if(PRIMES[center] < value)
+      i = center + 1;
+    else if(PRIMES[center] > value)
+      j = center - 1;
+    else
+      return center;
+  }
+  if(which == PREDECESSOR) {
+    if(PRIMES[i] < value)
+      return i;
+    else
+      return i-1;
+  } else if(which == SUCCESOR) {
+    if(PRIMES[i] < value)
+      return i+1;
+    else
+      return i;
+  }
+}
+
 long long *gap(int g, long long m, long long n) {
   if(isPRIMESInitialized == false)
     initPRIMES();
-  return NULL;
+  long long *result = (long long *) malloc(sizeof(long long)*2);
+  result[0] = 0;
+  result[1] = 0;
+  result[0] = binarySearchInPRIMES(m, SUCCESOR);
+  result[1] = binarySearchInPRIMES(n, PREDECESSOR);
+  return result;
 }
 
 int main(int argc, char *argv[]) {
-  gap(0, 0, 0);
+  long long *result = gap(0, 11, 23);
   for(int i = 0; i < 10; ++i)
     printf("%d ", PRIMES[i]);
+  printf("\nN = %d\n", N);
+  printf("%lld %lld\n", result[0], result[1]);
 }
