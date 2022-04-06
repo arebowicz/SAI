c5c4a8df913d264309ea13b919e1e6cf7c93a52c
arebowicz <arebowicz@gmail.com>
2022-04-07 00:57:43 +0200

    Next Bigger Number With The Same Digits

--- Next Bigger Number With The Same Digits/nextBiggerNumber.c
+++ Next Bigger Number With The Same Digits/nextBiggerNumber.c
@@ -1,12 +1,8 @@
 /* https://www.codewars.com/kata/55983863da40caa2c900004e/ */
-/* BRUTE-FORCE METHOD */
-/* TO OBSERVE SOME DEPENDENCIES AND PROPERTIES */
-/* AND ALSO TO TRAIN PROGRAMMING SKILL */
-/* BASIC TESTS - OK */
+/* ALL OF OVER 150 TESTS PASSED */
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdbool.h>
 
 /* n >= 0 */
 int intLen(long long n) {
@@ -19,73 +15,72 @@ int intLen(long long n) {
 }
 
 /* i >= 0 */
-int power10(int i) {
-  int j = 0, result = 1;
+long long power10(int i) {
+  int j = 0;
+  long long result = 1;
   while(j++ < i)
     result *= 10;
   return result;
 }
 
-static long long number;
-static int numLen;
-static long long next;
-static int *arrayOfDigits;
-
-void setArrayOfDigits() {
-  for(int i = 0; i < numLen; ++i)
-    arrayOfDigits[numLen - 1 - i] = (number / power10(i)) % 10;
+void setArrayOfDigits(int arrayOfDigits[], int arrLen, long long number) {
+  for(int i = 0; i < arrLen; ++i)
+    arrayOfDigits[arrLen - 1 - i] = (number / power10(i)) % 10;
   return;
 }
 
-/* number >= 0 */
-bool isDigitProper(int digit, long long number) {
-  int i = 0;
-  while(number/10 > 0) {
-    if(number % 10 == digit)
-      ++i;
-    number /= 10;
-  }
-  if(number % 10 == digit)
-    ++i;
-  int j = 0;
-  for(int k = 0; k < numLen; ++k)
-    if(arrayOfDigits[k] == digit)
-      ++j;
-  if(i < j)
-    return true;
-  else
-    return false;
+int findFirstSmallest(int arrayOfDigits[], int arrLen) {
+  int i;
+  for(i = arrLen - 1; i >= 0; --i)
+    for(int j = i + 1; j < arrLen; ++j)
+      if(arrayOfDigits[i] < arrayOfDigits[j])
+        return i;
+  return i;
 }
 
-void createNumberByRecursion(int actualRecursionLvl, long long actualNumber) {
-  if(actualRecursionLvl == numLen) {
-    if(intLen(actualNumber) == numLen && actualNumber > number
-          && actualNumber < next)
-      next = actualNumber;
-    return;
-  }
-  for(int i = 0; i < numLen; ++i) {
-    if(isDigitProper(arrayOfDigits[i], actualNumber))
-      createNumberByRecursion(actualRecursionLvl + 1,
-                                actualNumber * 10 + arrayOfDigits[i]);
-  }
+int findFirstGreater(int arrayOfDigits[], int arrLen, int i) {
+  for(int j = arrLen - 1; j > i; --j)
+    if(arrayOfDigits[i] < arrayOfDigits[j])
+      return j;
+  return -1;
+}
+
+void swap(int arrayOfDigits[], int i1, int i2) {
+  int tmp = arrayOfDigits[i1];
+  arrayOfDigits[i1] = arrayOfDigits[i2];
+  arrayOfDigits[i2] = tmp;
   return;
 }
 
-/* -1 when digits can't be rearranged to form a bigger number */
+void bubbleSortAscending(int arrayOfDigits[], int arrLen) {
+  for(int i = 0; i < arrLen - 1; ++i)
+    for(int j = 0; j < arrLen - 1 - i; ++j)
+      if(arrayOfDigits[j] > arrayOfDigits[j+1])
+        swap(arrayOfDigits, j, j+1);
+  return;
+}
+
+long long arrayToLongLong(int arrayOfDigits[], int arrLen) {
+  long long result = 0;
+  for(int i = 0; i < arrLen; ++i)
+    result = result*10 + arrayOfDigits[i];
+  return result;
+}
+
 long long nextBiggerNumber(long long n) {
-  number = n;
-  numLen = intLen(number);
-  arrayOfDigits = (int *) malloc(sizeof(int) * numLen);
+  int arrLen = intLen(n);
+  int *arrayOfDigits = (int *) malloc(sizeof(int) * arrLen);
   if(arrayOfDigits == NULL)
     return -1;
-  setArrayOfDigits();
-  next = number*10;
-  createNumberByRecursion(0, 0);
-  if(next == number*10)
+  setArrayOfDigits(arrayOfDigits, arrLen, n);
+  int firstSmallestIndex = findFirstSmallest(arrayOfDigits, arrLen);
+  if(firstSmallestIndex == -1)
     return -1;
-  else
-    return next;
+  int firstGreaterIndex = findFirstGreater(arrayOfDigits, arrLen, firstSmallestIndex);
+  swap(arrayOfDigits, firstSmallestIndex, firstGreaterIndex);
+  bubbleSortAscending(&arrayOfDigits[firstSmallestIndex + 1],
+                          arrLen - (firstSmallestIndex + 1));
+  return arrayToLongLong(arrayOfDigits, arrLen);
 }
 
 int main(int argc, char *argv[]) {
@@ -96,4 +91,9 @@ int main(int argc, char *argv[]) {
   printf("83752037 -> %lld\n", nextBiggerNumber(83752037));
   printf("97997977 -> %lld\n", nextBiggerNumber(97997977));
   printf("324 -> %lld\n", nextBiggerNumber(324));
+  printf("11276543 -> %lld\n", nextBiggerNumber(11276543));
+  printf("11576543 -> %lld\n", nextBiggerNumber(11576543));
+  printf("1043210 -> %lld\n", nextBiggerNumber(1043210));
+  printf("9876543210 -> %lld\n", nextBiggerNumber(9876543210));
+  printf("59884848459853 -> %lld\n", nextBiggerNumber(59884848459853));
 }
