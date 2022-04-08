85ba543b8abfa5397758cb9108b675c615f15a93
arebowicz <arebowicz@gmail.com>
2022-04-08 16:23:04 +0200

    Find The Smallest -- end of init. work, todo: main algorithm

--- Find The Smallest/findTheSmallest.c
+++ Find The Smallest/findTheSmallest.c
@@ -4,9 +4,59 @@
 
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <limits.h>
+
+#define MAX_NUM_OF_DIGITS 19
+
+int *arrayOfDigits;
+int arrLen;
+long long min;
+long long minIndex;
+long long insertIndex;
+
+void setArrayOfDigits(long long n);
+void findMin();
 
 long long *smallest(long long n) {
+  min = LLONG_MAX;
+  setArrayOfDigits(n);
+  findMin();
+  long long *result = (long long *) malloc(sizeof(long long) * 3);
+  result[0] = min;
+  result[1] = minIndex;
+  result[2] = insertIndex;
+  return result;
+}
 
+/* n >= 0 */
+int longLongLen(long long n) {
+  int i = 1;
+  while(n / 10 > 0) {
+    ++i;
+    n /= 10;
+  }
+  return i;
+}
+
+void setArrayOfDigits(long long n) {
+  arrLen = longLongLen(n);
+  if(arrLen > MAX_NUM_OF_DIGITS)
+    return;
+  arrayOfDigits = (int *) malloc(sizeof(int) * arrLen);
+  if(arrayOfDigits == NULL)
+    return;
+  int i = arrLen - 1;
+  while(i >= 0) {
+    arrayOfDigits[i--] = n % 10;
+    n /= 10;
+  }
+  return;
+}
+
+void findMin() {
+  return;
 }
 
 
@@ -18,12 +68,17 @@ void test(long long n, long long *exp) {
                              exp[0], exp[1], exp[2],          act[0], act[1], act[2] );
   assert(act[0] == exp[0] && act[1] == exp[1] && act[2] == exp[2]);
   free(act); act = NULL;
-  free(exp); exp = NULL;
+  return;
 }
 
 int main(int argc, char *argv[]) {
-  test(261235, (long long *) { 126235, 2, 0 });
-  test(209917, (long long *) { 29917, 0, 1 });
-  test(285365, (long long *) { 238565, 3, 1 });
-  test(296837, (long long *) { 239687, 4, 1 });
+  long long exp[][3] = { { 126235LL, 2LL, 0LL }, 
+                         { 29917LL, 0LL, 1LL },
+                         { 238565LL, 3LL, 1LL },
+                         { 239687LL, 4LL, 1LL } };
+  test(261235, exp[0]);
+  test(209917, exp[1]);
+  test(285365, exp[2]);
+  test(296837, exp[3]);
+  return 0;
 }
