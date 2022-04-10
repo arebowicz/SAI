c34ca58707e93780c9cbbe52442a7c79ef8fa865
arebowicz <arebowicz@gmail.com>
2022-04-10 20:15:25 +0200

    Magnet Particules In Boxes -- inefficient version

--- Magnet Particules In Boxes/summation.c
+++ Magnet Particules In Boxes/summation.c
@@ -1,16 +1,23 @@
 /* https://www.codewars.com/kata/56c04261c3fcf33f2d000534/ */
-/* TESTS PASSED: , FAILED: */
+/* TESTS PASSED: 10, FAILED: ? */
+/* TIME-OUT... */
+/* FOR "DOUBLE" (NOT "LONG DOUBLE") TESTS PASSED: ~50 */
 
 
+
+#include <math.h>
 
 double doubles(int maxk, int maxn) {
-
+  long double sum = 0.0;
+  for(int k = 1; k <= maxk; ++k)
+    for(int n = 1; n <= maxn; ++n)
+      sum += ((1 / (long double) k) * powl((long double) n + 1, (long double) -2 * k));
+  return (double) sum;
 }
 
 
 
 #include <stdio.h>
-#include <math.h>
 #include <stdbool.h>
 #include <assert.h>
 
@@ -32,7 +39,7 @@ void test(int maxk, int maxn, double expected) {
   return;
 }
 
-void tests(doubles, ShouldPassAllTheTestsProvided) {
+void tests() {
   test(1, 10, 0.5580321939764581);
   test(10, 1000, 0.6921486500921933);
   test(10, 10000, 0.6930471674194457);
